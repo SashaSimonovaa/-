0d85f3d33f37375704a77b06a6aff583b8de17d4
Александра <sasasimonova429@gmail.com>
2022-11-11 22:25:59 +0300

    .-.

--- пузырёк2.py
+++ пузырёк2.py
@@ -1,6 +1,13 @@
 a = [8, 6, 7, 3, 1, 4]
-for i in range(len(a)):
-    for j in range(len(a)-1, 0, -1):
+right = len(a)
+left = 0
+while right > left:
+    for j in range(right-1, left, -1):
         if a[j] < a[j-1]:
             a[j], a[j-1] = a[j-1], a[j]
+    right -= 1
+    for j in range(left, right+1):
+        if a[j] < a[j-1]:
+            a[j], a[j-1] = a[j-1], a[j]
+    left += 1
 print(*a)
